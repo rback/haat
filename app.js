8a58700d8a8c88ce43c8c2c37b8e8f8695b11076
Rasmus Back <rasmus.back@gmail.com>
2014-01-06 15:00:09 +0200

    Add preliminary templating and i18n support

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-var express = require("express");
+var express = require('express');
+var exphbs  = require('express3-handlebars');
+var i18n = require('i18n');
 var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
-app.use(express.static(__dirname + '/public'));
 
+app.configure(function () {
+	app.engine('.hbs', exphbs({
+		defaultLayout: 'default',
+		extname: '.hbs'
+	}));
+	app.set('views', "" + __dirname + "/views");
+	app.set('view engine', '.hbs');
+
+	i18n.configure({
+		locales: ['en', 'fi', 'se'],
+		cookie: 'locale',
+		updateFiles: false,
+		defaultLocale: 'fi',
+		directory: "" + __dirname + "/locales"
+	});
+
+	app.use(express.cookieParser());
+	app.use(i18n.init);
+	app.use(logfmt.requestLogger());
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function (req, res) {
+    res.render('main', {
+    	helpers: {
+            __: function (key) { return res.__(key); }
+        }
+    });
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
