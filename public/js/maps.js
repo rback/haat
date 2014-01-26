92ee44d53ca9c447a7818d339304ba4aba7ddd5d
Rasmus Back <rasmus.back@gmail.com>
2014-01-26 20:19:44 +0200

    Markers and better coordinates for places

--- public/js/maps.js
+++ public/js/maps.js
@@ -2,22 +2,46 @@ var showMap;
 
 $(function() {
     var locations = {
-        johanneksenkirkko: new google.maps.LatLng(60.162319, 24.945177),
-        g18: new google.maps.LatLng(60.165543, 24.942108),
-        hotels: new google.maps.LatLng(60.164486,24.933901)
+        johanneksenkirkko: {
+            center: new google.maps.LatLng(60.16184, 24.945013),
+            icon: "/img/rings.png"
+        },
+        g18: {
+            center: new google.maps.LatLng(60.165733, 24.941905),
+            icon: "/img/party.png"
+        },
+        radisson: {
+            center: new google.maps.LatLng(60.161628, 24.924819),
+            icon: "/img/hotel.png"
+        },
+        glo: {
+            center: new google.maps.LatLng(60.164222, 24.932082),
+            icon: "/img/hotel.png"
+        }
     }
 
     showMap = function showMap(location) {
         var mapElement = $("#map-" + location)
+        var center = locations[location].center
         mapElement.find('i').click(function(event) {
             mapElement.fadeOut()
         })
         mapElement.fadeIn()
         var map = new google.maps.Map(mapElement.find('.google-maps')[0], {
-            center: locations[location],
+            center: center,
             zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
         })
+        var image = {
+            url: locations[location].icon,
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(30, 83)
+        };
+        var marker = new google.maps.Marker({
+            position: center,
+            map: map,
+            icon: image,
+        });
     }
 
 	$('#navigation').localScroll();
