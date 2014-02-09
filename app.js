12ffa0b65bd58022efe73877f64e33c07c0f3d18
Rasmus Back <rasmus.back@gmail.com>
2014-02-09 16:39:15 +0200

    use double quotes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express'),
-	jade = require('jade'),
+var express = require("express"),
+	jade = require("jade"),
 	i18n = require("i18next"),
 	logfmt = require("logfmt"),
 	app = express(),
-	passport = require('passport'),
-	BasicStrategy = require('passport-http').BasicStrategy
+	passport = require("passport"),
+	BasicStrategy = require("passport-http").BasicStrategy
 
 passport.use(new BasicStrategy({}, function(username, password, done) {
     if (password == process.env.PASSWORD) {
     	console.log("PASSWORD: " + password + " is correct")
-    	return done(null, 'cuicca')    	
+    	return done(null, "cuicca")
     } else {
     	console.log("PASSWORD: " + password + " is incorrect")
     	return done(null, false)
@@ -17,17 +17,17 @@ passport.use(new BasicStrategy({}, function(username, password, done) {
 }))	
 
 app.configure(function () {
-	app.set('views', __dirname + "/views")
-	app.set('view engine', 'jade')
+	app.set("views", __dirname + "/views")
+	app.set("view engine", "jade")
 
 	i18n.init({
 		lng: "fi",
 		useCookie: true,
 		detectLngFromHeaders: false,
-		supportedLngs: ['fi', 'se', 'en'],
-		cookieName: 'locale',
-		fallbackLng: 'fi',
-		resGetPath: __dirname + '/locales/__lng__.json',
+		supportedLngs: ["fi", "se", "en"],
+		cookieName: "locale",
+		fallbackLng: "fi",
+		resGetPath: __dirname + "/locales/__lng__.json",
 		debug: false
 	})
 
@@ -35,19 +35,23 @@ app.configure(function () {
 	app.use(passport.initialize())
 	app.use(i18n.handle)
 	app.use(logfmt.requestLogger())
-	app.use(express.static(__dirname + '/public'))
+	app.use(express.static(__dirname + "/public"))
 })
 i18n.registerAppHelper(app)
 
-app.get('/',
-	passport.authenticate('basic', { session: false }),
+app.get("/",
+	passport.authenticate("basic", { session: false }),
 	function (req, res) {
-		res.render('index', { pretty: true })
+		res.render("index", { pretty: true })
 	}
 )
 
-app.get('/locale/:locale', function (req, res) {
-  res.cookie('locale', req.params.locale)
+app.get("/login", function(req, res) {
+	res.render("")
+})
+
+app.get("/locale/:locale", function (req, res) {
+  res.cookie("locale", req.params.locale)
   res.redirect("/")
 })
 
