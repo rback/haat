a92917fad7502d111961884d3973eafdc862d7bf
Rasmus Back <rasmus.back@gmail.com>
2014-02-16 19:56:59 +0200

    Deploy to heroku

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,5 @@
 
+$(function(){var n=$(".language"),a=n.find(".dropdown");n.click(function(){a.toggleClass("expanded")}),a.click(function(n){window.location="locale/"+$(n.target).data("language")})});
 $(function(){$(".loginbutton").click(function(){$("form").submit()})});
 var showMap;$(function(){var n={johanneksenkirkko:{center:new google.maps.LatLng(60.16184,24.945013),icon:"/img/rings.png"},g18:{center:new google.maps.LatLng(60.165733,24.941905),icon:"/img/party.png"},radisson:{center:new google.maps.LatLng(60.161628,24.924819),icon:"/img/hotel.png"},glo:{center:new google.maps.LatLng(60.164222,24.932082),icon:"/img/hotel.png"}};showMap=function(o){var e=$("#map-"+o),g=n[o].center;e.find("i").click(function(){e.fadeOut()}),e.fadeIn();{var a=new google.maps.Map(e.find(".google-maps")[0],{center:g,zoom:15,mapTypeId:google.maps.MapTypeId.ROADMAP}),i={url:n[o].icon,origin:new google.maps.Point(0,0),anchor:new google.maps.Point(30,83)};new google.maps.Marker({position:g,map:a,icon:i})}},$("#navigation").localScroll()});
 var popup;$(function(){popup=function(n,c){n.click(function(){c.fadeIn()}),c.find("i").click(function(){c.fadeOut()})}});
