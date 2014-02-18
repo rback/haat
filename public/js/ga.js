296a5ed953b415be0b8ce3a712ca945698172a9f
Rasmus Back <rasmus.back@reaktor.fi>
2014-02-18 19:42:50 +0200

    Fiddle with ga

--- public/js/ga.js
+++ public/js/ga.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-48158060-1', 'heidi-ja-rasm.us');
+ga('create', 'UA-48158060-1', 'auto');
 ga('send', 'pageview');
 
 window.onerror = function(message, url) {
